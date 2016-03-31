b521222f4762a6ed82cd4e2a67229a322320a9f5
shlgd1xs <150161107@qq.com>
2016-03-31 11:28:48 +0800

    create hello world

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Z-ben on 2016/3/31.
+ */
+var index = (function () {
+    function index() {
+    }
+    return index;
+})();
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by Z-ben on 2016/3/31.
+ */
+class index{
+
+}
